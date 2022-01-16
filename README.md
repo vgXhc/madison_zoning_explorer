929e623d8ee4257ae4dbf60b270d5f857582074c
vgXhc <kliems@gmail.com>
2022-01-15 18:32:55 -0600

    created basic framework for app. Added "abandoned" status to the readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # madison_zoning_explorer
 A Shiny app to explore where in Madison you can build/open specific types of businesses or housing
 
+# Development status
+Abandoned. The zoning code is so complex that I have just given up.
+
 ## Basic concept
 The app will use city of Madison zoning data from their open data portal as well as directly from ordinance to draw a map of where certain land uses are and are not permitted. 
 
@@ -9,3 +12,4 @@ The app will use city of Madison zoning data from their open data portal as well
 - [ ] link to zoning code definitions (or provide them in the app if linking is not possible)
 - [ ] select combination of land uses (e.g. "any multi-family housing" vs "only multi-family housing over x units") where that makes sense
 - [ ] information about parking requirements
+
--- madison_zoning_explorer/app.R
+++ madison_zoning_explorer/app.R
@@ -8,26 +8,35 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(sf)
+library(tmap)
+
+last_updated <- file.info("data/Zoning_Districts.shp")$ctime
+zoning <- read_sf("data/Zoning_Districts.shp")
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Where in Madison can I build/open a..."),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput("use", "what", choices = list(
+            "Bike shop" = "bike",
+            "Coffee shop" = "coffee"
+          ), 
+          selected = "bike"),
+            p("Zoning data last updated: ", last_updated)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("mapPlot")
         )
     )
 )
@@ -35,13 +44,9 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$mapPlot <- renderPlot({
+        tm_shape(zoning) +
+        tm_polygons("ZONING_COD")
     })
 }
 
